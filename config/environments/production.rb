6899609b4c460744352f82332d8c4fe6e84a2d8d
noracami <kerke.2011@gmail.com>
2025-03-04 18:29:56 +0800

    🐛 follow ActionDispatch::HostAuthorization
    ref: https://guides.rubyonrails.org/configuring.html#actiondispatch-hostauthorization

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  if deployment_host = ENV["DEPLOYMENT_HOST"].presence
+    config.host << deployment_host
+  end
 end
